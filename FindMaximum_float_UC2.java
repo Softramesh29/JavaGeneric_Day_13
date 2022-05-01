99546dacdf7bbbeb6670c89a079d2c4be4038409
softramesh29 <softramesh29@gmail.com>
2022-05-01 17:09:08 +0530

    Added : Finding maximum from the given 3 float values file added

---
+++ FindMaximum_float_UC2.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.JavaGeneric;
+
+public class FindMaximum_float_UC2 {
+	public void findMax(float x, float y, float z) {
+		float max = 0;
+		if (x>=y && x>=z) {
+			max = x;
+			System.out.println(x+" "+y+" "+z+" "+"Maximum is : "+max);
+		}	
+		if (y>=x && y>=z) {
+			max = y; 
+			System.out.println(x+" "+y+" "+z+" "+"Maximum is : "+max);
+		}
+		else {
+			max=z;
+			System.out.println(x+" "+y+" "+z+" "+"Maximum is : "+max);
+		}
+	}
+	public static void main(String [] args) {
+		FindMaximum_int_UC1 findMaximum_int_UC1 = new FindMaximum_int_UC1();
+		findMaximum_int_UC1.findMax(1.35, 3.64, 2.54);
+	}
+}
